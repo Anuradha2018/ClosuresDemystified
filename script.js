f1a5a09de994cc52fac18807c59260a186136c6f
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-21 20:48:17 +0100

    Snippet 1 uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+/*THis code below runs on a memory called stack, After the function execution is done, the memory gets disallocated */
+function Counter () {
+    let counter = 0;
+    counter ++;
+    console.log(counter);
+}
+Counter();// gives 1 as Output
+Counter();// gives 1 as Output
